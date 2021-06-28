547233d9e0861cd3af86ee2a0211e7187c65bf3b
Shariq Ahmed Khan <shariq@carbonteq.com>
2021-06-28 19:23:21 +0500

    refactor: Renamed actions as operators

--- app/redux/operators/UserActions.js
+++ app/redux/operators/UserActions.js
@@ -1,17 +1,13 @@
-import userServices from '../../api/UserServices';
-import {setUsers, appendUsers} from '../reducers/Users';
+import userServices from '../../services/UserServices';
+import {getUsers, setUsers, appendUsers, error} from '../reducers/Users';
 const DEBUG_KEY = '[UserActions]';
 import _ from 'lodash';
 
 export const getUsersData =
   (searchParams = {}) =>
   async (dispatch, getState) => {
+    dispatch(getUsers());
     const {filterOptions} = getState();
-
-    let nationalities = filterOptions.filter(option => option.selected);
-    nationalities = nationalities.map(nationality =>
-      nationality.dataCode.toLowerCase(),
-    );
 
     const contains = ({name, email}, query) => {
       const {first, last} = name;
@@ -32,7 +28,7 @@ export const getUsersData =
         result: {
           data: {results: usersData},
         },
-      } = await userServices.fetchUsers('', '', nationalities);
+      } = await userServices.fetchUsers(getNationalities(filterOptions));
 
       if (searchParams.formattedQuery) {
         usersData = _.filter(usersData, user => {
@@ -43,18 +39,28 @@ export const getUsersData =
       dispatch(setUsers({users: usersData}));
     } catch (err) {
       console.log(`${DEBUG_KEY} Error while getting users data:`, err);
+      dispatch(error({error: err}));
     }
   };
 
-export const appendUserData = () => async dispatch => {
+export const appendUserData = () => async (dispatch, getState) => {
+  dispatch(getUsers());
+  const {filterOptions} = getState();
+
   try {
-    let {
+    const {
       result: {
         data: {results: usersData},
       },
-    } = await userServices.fetchUsers();
+    } = await userServices.fetchUsers(getNationalities(filterOptions));
     dispatch(appendUsers({users: usersData}));
   } catch (err) {
     console.log(`${DEBUG_KEY} Error while getting users data:`, err);
+    dispatch(error({error: err}));
   }
+};
+
+const getNationalities = filterOptions => {
+  let nationalities = filterOptions.filter(option => option.selected);
+  return nationalities.map(nationality => nationality.dataCode.toLowerCase());
 };
